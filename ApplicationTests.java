b0362fe76926f8a8acb41254223f3c3dbf93c74e
Aastha Rajput <aastharajput967@gmail.com>
2026-02-06 23:33:10 -0500

    Fix import statements in ApplicationTests.java

--- ApplicationTests.java
+++ ApplicationTests.java
@@ -3,7 +3,10 @@ package com.aastharajput;
 import {useState, useEffects} from React;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class ApplicationTests {
